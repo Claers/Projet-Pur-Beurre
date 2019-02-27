30e7becd46b95a016d873f4caa91f9e86fd32cba
Unknown <ferrereflorent@hotmail.fr>
2019-02-27 18:34:53 +0400

    Test debug #2

--- PurBeurre/settings.py
+++ PurBeurre/settings.py
@@ -150,11 +150,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'   # Qui devrait déjà être la configuration par défaut
 if ON_PROD or ON_HEROKU:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 else:
     STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, "staticfiles"),
+        os.path.join(BASE_DIR, "static"),
     )
 
 
